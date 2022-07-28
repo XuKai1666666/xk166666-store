ba40c830f8a51a854058f732c7969a1bc597ee7a
Xukai1666666 <2684024189@qq.com>
2022-07-29 00:40:56 +0800

    更改为script setup写法，更改icon，添加Switch按钮，分离menu到 ShopDrodown.vue

---
+++ src/view/HomePage.vue
@@ -0,0 +1,65 @@
+<template>
+  <a-layout>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu v-model:selectedKeys="selectedKeys1" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+        <a-menu-item key="1">nav 1</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+
+      </a-menu>
+      <div style="{ 
+        width: 50px;
+        height: 50px;
+        float: right; }">
+        <a-switch v-model:checked="checked" />
+      </div>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <a-breadcrumb style="margin: 16px 0">
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item>List</a-breadcrumb-item>
+        <a-breadcrumb-item>App</a-breadcrumb-item>
+      </a-breadcrumb>
+      <a-layout style="padding: 24px 0; background: #fff">
+        <a-layout-sider width="200" style="background: #fff">
+        
+          <shop-dropdown />
+
+        </a-layout-sider>
+        <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+          Content
+        </a-layout-content>
+      </a-layout>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
+</template>
+<script setup lang="ts">
+
+import { ref } from 'vue';
+import ShopDropdown from '../components/ShopDropdown.vue';
+const checked = ref<boolean>(false);
+const selectedKeys1 = ref<string[]>(['2']);
+
+</script>
+<style>
+#components-layout-demo-top-side .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
